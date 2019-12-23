da5cb5e8606878ff7f3b1be7fa03c934d14dc9e3
Yurii_Kosygin <ykosygin@mail.ru>
2019-12-23 23:13:15 +0300

    add webpack config

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,8 +1,16 @@
-import { configure } from '@storybook/react';
+import React from 'react'
+import { configure, addDecorator } from '@storybook/react';
 import requireContext from 'require-context.macro';
+import { addReadme } from 'storybook-readme';
 import 'antd/dist/antd.less';
 
-import '../src/index.css';
+const styles = {
+	padding: '20px 40px',
+};
+
+const CenterDecorator = storyFn => <div style={styles}>{storyFn()}</div>;
+addDecorator(CenterDecorator);
+addDecorator(addReadme);
 
 const req = requireContext('../src/stories', true, /\.stories\.js$/);
 
--- src/stories/ButtonLocal.stories.js
+++ src/stories/ButtonLocal.stories.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import { storiesOf } from '@storybook/react';
+import { storiesOf  } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
-import { DatePicker } from 'antd';
+import { Button } from 'antd';
+import readme from '../components/README.md'
 
 export const text = 'Button';
 
@@ -9,5 +10,19 @@ export const actions = {
 	onClick: action('onClick'),
 };
 
-storiesOf('Buttons Local', module)
-	.add('datepicker', () => <DatePicker />);
+storiesOf('Buttons', module)
+	.addParameters({
+		readme: {
+			// override docs
+			content: readme,
+			sidebar: readme,
+		},
+	})
+	.add('primary', () => (
+		<div>
+			<Button type="primary">primary</Button>
+		</div>))
+	.add('default', () => <Button {...actions}>default</Button>)
+	.add('dashed', () => <Button {...actions} type="dashed">dashed</Button>)
+	.add('danger', () => <Button {...actions} type="danger">danger</Button>)
+	.add('link', () => <Button {...actions} type="link">link</Button>);
